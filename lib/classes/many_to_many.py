7f4c1ce34cc61f88f390317c6166beeb66bee3eb
Victor Leina <mtgrt1369@gmail.com>
2024-06-02 08:53:50 +0300

    tried

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,96 @@
 class Article:
+    all = []
     def __init__(self, author, magazine, title):
+        self._title = title
         self.author = author
         self.magazine = magazine
-        self.title = title
-        
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        raise AttributeError("Title is immutable")
+   
+
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        self._name = name
+        self._articles = []
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        raise AttributeError("Name is immutable")
+
+    @property
     def articles(self):
-        pass
-
-    def magazines(self):
-        pass
+        return self._articles
 
     def add_article(self, magazine, title):
-        pass
+        article = Article(self, magazine, title)
+        self._articles.append(article)
+        return article
 
-    def topic_areas(self):
-        pass
+    @property
+    def magazines(self):
+        return list(set(article.magazine for article in self._articles))
 
 class Magazine:
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        self._name = name
+        self._category = category
+        self._articles = []
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be of type str")
+        if not (2 <= len(value) <= 16):
+            raise ValueError("Name must be between 2 and 16 characters, inclusive")
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Category must be of type str")
+        if len(value) == 0:
+            raise ValueError("Category must be longer than 0 characters")
+        self._category = value
+
+    @property
     def articles(self):
-        pass
+        return self._articles
 
+    def add_article(self, article):
+        if isinstance(article, Article):
+            self._articles.append(article)
+
+    @property
     def contributors(self):
-        pass
+        return list(set(article.author for article in self._articles))
 
     def article_titles(self):
-        pass
+        return [article.title for article in self._articles]
 
     def contributing_authors(self):
-        pass
+        author_counts = {}
+        for article in self._articles:
+            author = article.author
+            if author not in author_counts:
+                author_counts[author] = 0
+            author_counts[author] += 1
+        return [author for author, count in author_counts.items() if count > 2]
--- lib/debug.py
+++ lib/debug.py
@@ -5,9 +5,81 @@ from classes.many_to_many import Article
 from classes.many_to_many import Author
 from classes.many_to_many import Magazine
 
-if __name__ == '__main__':
-    print("HELLO! :) let's debug :vibing_potato:")
+class Author:
+    def __init__(self, name):
+        self._name = name
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
+
+    def articles(self):
+        return self._articles
+
+    def magazines(self):
+        return list(set([article.magazine for article in self._articles]))
+
+    def add_article(self, magazine, title):
+        article = Article(self, magazine, title)
+        self._articles.append(article)
+        return article
+
+    def topic_areas(self):
+        return list(set([article.magazine.category for article in self._articles]))
 
 
-    # don't remove this line, it's for debugging!
-    ipdb.set_trace()
+class Magazine:
+    def __init__(self, name, category):
+        self._name = name
+        self._category = category
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def category(self):
+        return self._category
+
+    def articles(self):
+        return self._articles
+
+    def contributors(self):
+        return list(set([article.author for article in self._articles]))
+
+    def article_titles(self):
+        return [article.title for article in self._articles]
+
+    def contributing_authors(self):
+        authors_count = {}
+        for article in self._articles:
+            authors_count[article.author] = authors_count.get(article.author, 0) + 1
+        return [author for author, count in authors_count.items() if count > 2]
+
+    @classmethod
+    def top_publisher(cls):
+        all_magazines = []  # You need to maintain a list of all Magazine instances somewhere
+        if not all_magazines:
+            return None
+        return max(all_magazines, key=lambda magazine: len(magazine.articles()))
+
+
+class Article:
+    def __init__(self, author, magazine, title):
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author(self):
+        return self._author
+
+    @property
+    def magazine(self):
+        return self._magazine
